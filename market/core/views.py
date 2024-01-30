41789415a7b34bc5ee5f3e82af182ebd6864b2e8
M-Desktop <m_t020297@outlook.com>
2024-01-29 20:24:01 -0500

    touched up details page

--- market/core/views.py
+++ market/core/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from item.models import Category, Item
 
 def index(request):
-    items = Item.objects.filter(is_sold=False)[0:1]
+    items = Item.objects.filter(is_sold=False)[0:2]
     categories = Category.objects.all()
 
     return render(request, 'core/index.html', {
